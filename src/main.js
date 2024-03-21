76e20e1c0170e3f06c0503b6054d5663162b5d7e
Nurunnahar <nurunnahar9081@gmail.com>
2024-03-21 14:50:50 +0600

    validation done

---
+++ src/main.js
@@ -0,0 +1,18 @@
+const student_create_form = document.getElementById("student-create-form");
+const msg = document.querySelector(".msg");
+
+student_create_form.onsubmit = (e) => {
+  e.preventDefault();
+
+  //get from data
+  const form_data = new FormData(e.target);
+  const { name, email, phone, location, photo } = Object.fromEntries(form_data);
+  
+    if (!email || !phone || !location || !name || !photo) {
+      msg.innerHTML = createAlert("All fields are required");
+    } else if (!isEmail(email)) {
+      msg.innerHTML = createAlert("Please enter a valid email", "warning");
+    } else if (!isPhone(phone)) {
+      msg.innerHTML = createAlert("Please enter a valid Mobile Number", "warning");
+    } 
+};
